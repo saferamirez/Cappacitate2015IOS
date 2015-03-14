932424033874a0d6d32a9ee111291d0432b1b558
Felipe Ramirez Mac <safe.ramirez@gmail.com>
2015-03-14 11:07:08 -0500

    Primer Commit

---
+++ CedoArriendoTests/CedoArriendoTests.m
@@ -0,0 +1,40 @@
+//
+//  CedoArriendoTests.m
+//  CedoArriendoTests
+//
+//  Created by Saul Felipe Ramirez Rojas on 14/03/15.
+//  Copyright (c) 2015 SafeDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CedoArriendoTests : XCTestCase
+
+@end
+
+@implementation CedoArriendoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
