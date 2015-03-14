def0221bfb13e394a6a80da33ebfb48bb7443c8a
Felipe Ramirez Mac <safe.ramirez@gmail.com>
2015-03-14 13:22:21 -0500

    Facebook registro

---
+++ CedoArriendo/FacebookViewController.h
@@ -0,0 +1,14 @@
+//
+//  FacebookViewController.h
+//  CedoArriendo
+//
+//  Created by Saul Felipe Ramirez Rojas on 14/03/15.
+//  Copyright (c) 2015 SafeDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FacebookViewController : UIViewController <UIWebViewDelegate>
+@property (strong, nonatomic) IBOutlet UIWebView *navegador;
+
+@end
